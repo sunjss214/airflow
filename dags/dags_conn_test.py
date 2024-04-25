11585f811af1c58b7d2f2122dde6486104608897
sunjss <sunjss214@gmail.com>
2024-04-25 22:07:34 +0900

    테스트수정2

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_conn_test",
     schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchum=false
+    catchup=False
 ) as dag:
     
     t1= EmptyOperator(
